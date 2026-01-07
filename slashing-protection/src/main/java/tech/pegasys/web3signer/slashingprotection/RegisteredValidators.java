def51a4d81db483f4f9666a9ca4a09dc1e4461ce
s. rannou <mxs@sbrk.org>
2026-01-07 09:50:29 +0100

    fix: race condition in keymanager API
    The BiMap used to have a map of public key to db indexes is not
    properly synchronized: it can effectively be accessed from multiple
    threads in parallel:
    
    - in read-only from signing requests from validator clients (1),
    - in a writable-way from the key manager API (2).
    
    This PR ensures this is synchronized, this implies some contention
    especially around (1) which is why it's using a readwrite lock
    approach.

--- slashing-protection/src/main/java/tech/pegasys/web3signer/slashingprotection/RegisteredValidators.java
+++ slashing-protection/src/main/java/tech/pegasys/web3signer/slashingprotection/RegisteredValidators.java
@@ -21,6 +21,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.HashBiMap;
@@ -32,6 +34,7 @@ import org.jdbi.v3.core.Jdbi;
 public class RegisteredValidators {
   private static final Logger LOG = LogManager.getLogger();
   private final BiMap<Bytes, Integer> registeredValidators;
+  private final ReadWriteLock validatorsLock = new ReentrantReadWriteLock();
   private final Jdbi jdbi;
   private final ValidatorsDao validatorsDao;
 
@@ -49,15 +52,30 @@ public class RegisteredValidators {
   }
 
   public Set<Integer> validatorIds() {
-    return Collections.unmodifiableSet(registeredValidators.values());
+    validatorsLock.readLock().lock();
+    try {
+      return Collections.unmodifiableSet(registeredValidators.values());
+    } finally {
+      validatorsLock.readLock().unlock();
+    }
   }
 
   public Optional<Bytes> getPublicKeyForValidatorId(final int validatorId) {
-    return Optional.ofNullable(registeredValidators.inverse().get(validatorId));
+    validatorsLock.readLock().lock();
+    try {
+      return Optional.ofNullable(registeredValidators.inverse().get(validatorId));
+    } finally {
+      validatorsLock.readLock().unlock();
+    }
   }
 
   public Optional<Integer> getValidatorIdForPublicKey(final Bytes publicKey) {
-    return Optional.ofNullable(registeredValidators.get(publicKey));
+    validatorsLock.readLock().lock();
+    try {
+      return Optional.ofNullable(registeredValidators.get(publicKey));
+    } finally {
+      validatorsLock.readLock().unlock();
+    }
   }
 
   public int mustGetValidatorIdForPublicKey(final Bytes publicKey) {
@@ -78,7 +96,12 @@ public class RegisteredValidators {
 
     LOG.info("Validators registered successfully in database:{}", registeredValidatorsList.size());
 
-    registeredValidatorsList.forEach(
-        validator -> registeredValidators.put(validator.getPublicKey(), validator.getId()));
+    validatorsLock.writeLock().lock();
+    try {
+      registeredValidatorsList.forEach(
+          validator -> registeredValidators.put(validator.getPublicKey(), validator.getId()));
+    } finally {
+      validatorsLock.writeLock().unlock();
+    }
   }
 }
